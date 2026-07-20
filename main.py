7ce0759c0238d064c0170babaf5b8925c4c8dc2d
Joseph <joseph.hansjpedronan@gmail.com>
2026-07-20 14:00:13 -0700

    vid10

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import psycopg2, secrets
 from flask import Flask, render_template, request, session
 
-conn = psycopg2.connect(database="postgres",user="postgres",password="pass",host="localhost",port="5432")
+conn = psycopg2.connect(database="postgres",user="postgres",password="admin",host="localhost",port="5432")
 cursor = conn.cursor()
 
 app = Flask(__name__)
@@ -10,3 +10,9 @@ app.secret_key = secrets.token_hex(16)
 @app.route('/', methods = ['POST', 'GET'])
 def sign_up_page():
     return render_template('sign-up.html')
+
+@app.route('/sign-in', methods = ['POST', 'GET'])
+def sign_in_page():
+    return render_template("sign-in.html")
+
+app.run()
