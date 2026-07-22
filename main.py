7da11fa5b53f2cf90ba9284041073c22e6bd3509
Joseph <joseph.hansjpedronan@gmail.com>
2026-07-21 17:32:48 -0700

    15

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ cursor = conn.cursor()
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 
-@app.route('/', methods = ['POST', 'GET'])
+@app.route('/', methods = ['POST','GET'])
 def sign_up_page():
     return render_template('sign-up.html')
-
-@app.route('/sign-in', methods = ['POST', 'GET'])
+  
+@app.route('/sign-in', methods = ['POST','GET'])
 def sign_in_page():
-    return render_template("sign-in.html")
+  return render_template("sign-in.html")
 
 @app.route('/shop', methods = ['POST'])
 def shop_page():
@@ -26,16 +26,18 @@ def shop_page():
             cursor.execute(f"INSERT INTO users (name,password,email) VALUES ('{session['username']}','{password}','{email}')")
             conn.commit()
             return render_template('shop.html', name = session['username'])
-        elif request.referrer == "http://localhost:5000/sign-in":
+        elif request.referrer == "http://localhost:5000/sign-in": 
             session['username'] = request.form['username']
             password = request.form['password']
             cursor.execute(f"SELECT password FROM users where name = '{session['username']}'")
             real_password = cursor.fetchall()
             if real_password[0][0] == password:
-                return render_template('shop.html', name = session['username'])
+                cursor.execute(f"SELECT departure,arrival,departure_time,arrival_time FROM tickets WHERE users = '{session['username']}'")
+                user_existing_flights = cursor.fetchall()
+                return render_template('shop.html', name = session['username'], flights = user_existing_flights)
             else:
                 return render_template('wrong.html')
-
+              
 @app.route('/ticket', methods = ['POST'])
 def ticket_page():
     if request.method == 'POST':
@@ -44,34 +46,23 @@ def ticket_page():
             destination = request.form['buy-destination']
             cursor.execute(f"SELECT * FROM flights WHERE arrival_state = '{destination}' AND departure_state = '{departure}'")
             flight_match = cursor.fetchall()
-            flight_object = {"departure": flight_match[0][1], 
-                             "destination": flight_match[0][2], 
-                             "departure_time": flight_match[0][3].strftime("%Y-%m-%d %H:%M"), 
-                             "arrival_time": flight_match[0][4].strftime("%Y-%m-%d %H:%M"),
-                             "price": flight_match[0][5]}
+            flight_object = {"departure": flight_match[0][1], "destination": flight_match[0][2], "departure_time": flight_match[0][3].strftime("%Y-%m-%d %H:%M"), "arrival_time": flight_match[0][4].strftime("%Y-%m-%d %H:%M"), "price": flight_match[0][5]}
             return render_template('ticket.html', available_flights = flight_object, action = 'buy')
-
-def generate_ticket_id():
-    length = 10
-    chars = string.ascii_lowercase + string.digits
-    return ''.join(random.choice(chars) for _ in range(length))
+        elif 'delete-departure' in request.form:
+           session['delete-departure'] = request.form['delete-departure']
+           session['delete-destination'] = request.form['delete-destination']
+           cursor.execute(f"SELECT * FROM tickets WHERE arrival = '{session['delete-destination']}' AND departure = '{session['delete-departure']}' AND users = '{session['username']}'")
+           flight_match = cursor.fetchall()
+           return render_template('ticket.html', flight = flight_match, action = 'delete')
 
 @app.route('/payment', methods = ['POST'])
 def purchase_page():
     if request.method == 'POST':
-        session['user_departure'] = request.form['departure']
-        session['user_destination'] = request.form['destination']
-        session['user_departure_time'] = request.form['departure_time']
-        session['user_arrival_time'] = request.form['arrival_time']
-        session['user_price'] = request.form['price']
-        return render_template('payment.html')
-app.run()
+      session['user_departure'] = request.form['departure']
+      session['user_destination'] = request.form['destination']
+      session['user_departure_time'] = request.form['departure_time']
+      session['user_arrival_time'] = request.form['arrival_time']
+      session['user_price'] = request.form['price']
+      return render_template('payment.html')
 
-@app.route("/success", methods = ['POST'])
-def success_page():
-    if request.method == 'POST':
-        ticket_id = generate_ticket_id()
-        cursor.execute(f"INSERT INTO tickets (ticket_number,departure,arrival,departure_time,arrival_time,ticket_price,users) VALUES ('{ticket_id}','{session["user_departure"]}','{session["user_destination"]})','{session['user_departure_time']}','{session['user_price']}','{session['username']}')")
-        conn.commit()
-        return render_template("success.html", ticket_id = ticket_id)
 app.run()
